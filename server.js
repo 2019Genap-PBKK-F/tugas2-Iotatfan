bc7b9769b4f7219388a1dca83307546098048a27
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-02-19 12:17:31 +0700

    Init

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const hostname = '10.199.14.46';
+const port = 8012;
+
+//Create HTTP server and listen on port 8012 for requests
+http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Halo Dunia\n');
+}).listen(port);
+
+console.log(`Server running at http://${hostname}:${port}/`);
