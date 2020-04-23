cd918484d9863a7f4f5eb3c5bb21d18212340ff4
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-04-23 19:40:58 +0700

    edit master indikator

--- server.js
+++ server.js
@@ -250,6 +250,7 @@ app.get("/api/master-indikator/", function(req, res)
 app.post("/api/master-indikator/", function(req, res)
 {
   var model = [
+    { name: 'id_aspek', sqltype: sql.Int, value: req.body.id_aspek },
     { name: 'id_penyebut', sqltype: sql.Int, value: req.body.id_penyebut },
     { name: 'id_pembilang', sqltype: sql.Int, value: req.body.id_pembilang },
     { name: 'nama', sqltype: sql.VarChar, value: req.body.nama },
@@ -258,8 +259,8 @@ app.post("/api/master-indikator/", function(req, res)
     { name: 'expired_date', sqltype: sql.DateTime, value: req.body.expired_date }
   ]
 
-  var query = "insert into MasterIndikator( id_penyebut, id_pembilang, nama, deskripsi, default_bobot, create_date, last_update, expired_date ) "
-              + "values( @id_penyebut, @id_pembilang, @nama, @deskripsi, @default_bobot, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, (CURRENT_TIMESTAMP+365) )"
+  var query = "insert into MasterIndikator( id_aspek, id_penyebut, id_pembilang, nama, deskripsi, default_bobot, create_date, last_update, expired_date ) "
+              + "values( @id_aspek, @id_penyebut, @id_pembilang, @nama, @deskripsi, @default_bobot, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, (CURRENT_TIMESTAMP+365) )"
   executeQuery(res, query, model, 1)
 })
 
@@ -267,6 +268,7 @@ app.put("/api/master-indikator/:id", function(req, res)
 {
   var model = [
     { name: 'id', sqltype: sql.Int, value: req.body.id },
+    { name: 'id_aspek', sqltype: sql.Int, value: req.body.id_aspek },
     { name: 'id_penyebut', sqltype: sql.Int, value: req.body.id_penyebut },
     { name: 'id_pembilang', sqltype: sql.Int, value: req.body.id_pembilang },
     { name: 'nama', sqltype: sql.VarChar, value: req.body.nama },
@@ -274,7 +276,7 @@ app.put("/api/master-indikator/:id", function(req, res)
     { name: 'default_bobot', sqltype: sql.Float, value: req.body.default_bobot }
   ]
 
-  var query = "update MasterIndikator set id_penyebut = @id_penyebut, id_pembilang = @id_pembilang, nama = @nama, deskripsi = @deskripsi, "
+  var query = "update MasterIndikator set id_aspek = @id_aspek, id_penyebut = @id_penyebut, id_pembilang = @id_pembilang, nama = @nama, deskripsi = @deskripsi, "
               + "default_bobot = @default_bobot, last_update = CURRENT_TIMESTAMP where id = @id"
   executeQuery(res, query, model, 1)
 })
