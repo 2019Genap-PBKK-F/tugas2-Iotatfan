2ac7d221bd499acdfd5d9d13f2a6b1cf917d677f
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-05-08 14:11:32 +0700

    add new routes

--- server.js
+++ server.js
@@ -520,6 +520,31 @@ app.get("/api/log-indikator-satker", function(req, res){
   executeQuery(res, query, null, 0)
 })
 
+//Read FP
+app.get("/api/dosen/", function(req, res)
+{
+    var query = "select * from Dosen"
+    executeQuery(res, query, null, 0)
+})
+
+app.get("/api/abmas/", function(req, res)
+{
+    var query = "select * from abmas"
+    executeQuery(res, query, null, 0)
+})
+
+app.get("/api/publikasi/", function(req, res)
+{
+    var query = "select * from publikasi"
+    executeQuery(res, query, null, 0)
+})
+
+app.get("/api/penelitian/", function(req, res)
+{
+    var query = "select * from penelitian"
+    executeQuery(res, query, null, 0)
+})
+
 //  LISTEN
 
 app.listen(port, hostname, function () {
